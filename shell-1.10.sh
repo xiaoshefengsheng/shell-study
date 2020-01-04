41e21bd5c1f53b0bf264b3b4fbe9eb51257a22cc
xiaoshefengsheng <xiaoshefengsheng@gmail.com>
2020-01-04 22:50:23 +0800

    修改为倒计时30s

--- shell-1.10.sh
+++ shell-1.10.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #文件名： sleep.sh  可以实现计时器的数字变动,tput sc存储光标位置，tput rc恢复光标位置，tput ed删除光标到行尾内容，echo -n 只输出到当前行,sleep 1实现时间延时
-echo -n Count:
+echo -n 倒计时30s,还有:
 tput sc
 
-count=0;
+#count=0;
+count=30;
 while true;
 do
-    if [ $count -lt 30 ];
+    if [ $count -ge 0 ];
     then
-        let count++;
+        let count--;
         sleep 1;
         tput rc
         #tput ed
